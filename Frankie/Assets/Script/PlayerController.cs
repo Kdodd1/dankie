95d0442ca7a4f27183f36f079e05c66b60982831
Kyle <Kylepdodd1@gmail.com>
2018-07-25 18:39:06 -0500

    init commit

---
+++ Frankie/Assets/Script/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float xScale;
+    public float yScale;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+   
+
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+
+
+    }
+    private void Update(){
+
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+        
+        
+    }
+    private void FixedUpdate(){
+
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Enemy") {
+            transform.localScale += new Vector3(xScale, yScale, 0);
+            Destroy(collision.gameObject);
+        }
+    }public void getxScale(){
+    
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+"# Frank"  
+"New Line Test" 
+'deez nutts' 
