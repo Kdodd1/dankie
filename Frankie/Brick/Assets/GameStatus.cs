f2bfbef49fcb5b5917921ba28072cf882d14c94e
RyukyuSpirit <sdavis3491@gmail.com>
2018-08-05 15:28:04 -0400

    temp adding brick breaker

---
+++ Frankie/Brick/Assets/GameStatus.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStatus : MonoBehaviour {
+
+    //config params
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlock = 83;
+    public Text scoreText;
+
+    //state variables
+    [SerializeField] int currentScore = 0;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Time.timeScale = gameSpeed;
+	}
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlock;
+        scoreText.text = currentScore.ToString();
+    }
+}
---
+++ Frankie/Brick/Assets/Scripts/Block.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+    [SerializeField] AudioClip breakSound;
+    Level level;
+    GameStatus status;
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        status = FindObjectOfType<GameStatus>();
+        level.CountBreakableBlocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        status.AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        level.BlockDestroyed();
+        Destroy(gameObject);
+    }
+}
